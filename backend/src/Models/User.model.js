c4d1f6ce2f6a08d67084c1b530f9bab1ef2eee96
iamatultiwwari <atult2386@gmail.com>
2026-02-06 14:10:07 +0530

    added model & also bcrypt for hashing the password

---
+++ backend/src/Models/User.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import {Schema} from "mongoose";
+
+ const UserSchema  = new Schema(
+    {
+        Username:{
+            type:String,
+            required:true,
+        },
+        password:{
+            type:String,
+            requred:[true,"password is required"]
+        },
+        email:{
+            type:String,
+            required:[true,"please enter the email"]
+        },
+        fullname:{
+            type:String,
+            trim:true,
+            index:true,
+            required:true
+        }
+    },[timestamp = true]
+ );
+// check if - not modified then return  next
+// Hash password before saving user
+
+
+UserSchema.pre("save", async function (next) {
+    if(!this.isModified('password')) return next();
+  this.password =   await bcrypt.hash(this.password,10)
+    next()
+})
+
+
+UserSchema.methods.isPasswordCorrect =  async function (password) {
+   return  await bcrypt.compare(password,this.password)
+}
+
